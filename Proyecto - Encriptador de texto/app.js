89e66fe696ce6d7143ffc78d1fe7ed69a49dadd8
milorco <119813155+milorco@users.noreply.github.com>
2024-08-27 20:25:32 -0600

    Desarrollo de encriptador de texto

---
+++ Proyecto - Encriptador de texto/app.js
@@ -0,0 +1,51 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("principal-img");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "ede")
+      .replace(/i/gi, "igt")
+      .replace(/a/gi, "abe")
+      .replace(/o/gi, "oter")
+      .replace(/u/gi, "umer");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/encriptado.jpg";
+    } else {
+      muñeco.src = "./img/principal.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("principal-img");
+  
+    let textoCifrado = texto
+      .replace(/ede/gi, "e")
+      .replace(/igt/gi, "i")
+      .replace(/abe/gi, "a")
+      .replace(/oter/gi, "o")
+      .replace(/umer/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+      } else {
+        muñeco.src = "./img/principal.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
